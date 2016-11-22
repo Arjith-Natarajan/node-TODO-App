6f48cba6dc616568b16dc91aad40486ebe587d8e
Arjith Natarajan <arjith496@gmail.com>
2016-11-22 23:52:05 +0530

    Set up Webpage in localhost
    created module and controller in angulaerjs
    included bootstrap and angular js for view generation

---
+++ public/core.js
@@ -0,0 +1,40 @@
+var myTodo = angular.module('myTodo', []);
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    //when landign we need to get all todos and show them on the page
+    $http.get('/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    $scope.createTodo = function() {
+
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {};
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+}
